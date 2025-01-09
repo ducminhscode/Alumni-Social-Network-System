8424cb965f04f651f5fc39af2bae70cdb944554f
Duc Minh <ducmin76@gmail.com>
2025-01-10 03:56:12 +0700

    small update

--- MangXaHoiCuuSinhVien/social_media_app/social_media_app/urls.py
+++ MangXaHoiCuuSinhVien/social_media_app/social_media_app/urls.py
@@ -14,12 +14,34 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
 from django.urls import path, include, re_path
 
 from app.admin import my_admin_site
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="App API",
+        default_version="v1",
+        description="APIs for App",
+        contact=openapi.Contact(email=os.getenv('EMAIL_SEND')),
+        license=openapi.License(name="Social Media App"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
 
 urlpatterns = [
     path('',include('app.urls')),
     path('admin/', my_admin_site.urls),
+    path('api/',include('app.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
